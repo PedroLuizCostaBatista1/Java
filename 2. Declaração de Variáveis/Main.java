af123401a6bcf75d96b6283108ed64930e190ba7
Pedro Luiz <0001156035@senaimgaluno.com.br>
2025-10-30 08:36:26 -0300

    Criação do repositorio

---
+++ 2. Declaração de Variáveis/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int idade = 19;
+        double altura = 1.75;
+        char genero = 'M';
+        boolean presente = true;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura + "m");
+        System.out.println("Genero: " + genero);
+        System.out.println("Presente? " + presente);
+    }
+}
+ 
